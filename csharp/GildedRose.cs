671db1af7f6c05b13c503496fd28adb941fcd0e8
David betancourt <david@atarraya.ai>
2024-05-31 08:57:34 -0600

    Agregar archivos de proyecto.

---
+++ csharp/GildedRose.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach(Item item in Items)
+            {
+                item.SellIn--;
+                switch (item.Name)
+                {
+                    case "Aged Brie":
+                        UpdateAgedBrie(item);
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        UpdateBackstagePasses(item);
+                        break;
+                    default:
+                        if (item.Name.Contains("Conjured"))
+                            UpdateConjuredItem(item);
+                        else
+                            UpdateNormalItem(item);
+                        break;
+                }
+            }
+
+        }
+
+        private void UpdateAgedBrie(Item item)
+        {
+            item.Quality = Math.Min(50, item.Quality + 1);
+            if (item.SellIn < 0)
+                item.Quality = Math.Min(50, item.Quality + 1);
+        }
+
+        private void UpdateBackstagePasses(Item item)
+        {
+            if (item.SellIn < 0)
+                item.Quality = 0;
+            else if (item.SellIn < 6)
+                item.Quality = Math.Min(50, item.Quality + 3);
+            else if (item.SellIn < 11)
+                item.Quality = Math.Min(50, item.Quality + 2);
+            else
+                item.Quality = Math.Min(50, item.Quality + 1);
+        }
+
+        private void UpdateConjuredItem(Item item)
+        {
+            item.Quality = Math.Max(0, item.Quality - 2);
+            if (item.SellIn < 0)
+                item.Quality = Math.Max(0, item.Quality - 2);
+        }
+
+        private void UpdateNormalItem(Item item)
+        {
+            item.Quality = Math.Max(0, item.Quality - 1);
+            if (item.SellIn < 0)
+                item.Quality = Math.Max(0, item.Quality - 1);
+        }
+    }
+}
---
+++ test/GildedRoseTest.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+
+        [Test]
+        public void UpdateQuality_AgedBrie_IncreasesQuality()
+        {
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 20 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(21, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_AgedBrie_QualityNeverExceeds50()
+        {
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 49 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(50, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_BackstagePasses_IncreasesQuality()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(22, items[0].Quality);
+        }
+
+
+        [Test]
+        public void UpdateQuality_BackstagePasses_QualityNeverExceeds50()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(50, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_BackstagePasses_QualityDropsToZeroAfterConcert()
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(0, items[0].Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_ConjuredItem_DecreasesQualityTwiceAsFast()
+        {
+            var items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 5, Quality = 20 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(18, items[0].Quality);
+        }        
+
+        [Test]
+        public void UpdateQuality_NormalItem_DecreasesQuality()
+        {
+            var items = new List<Item> { new Item { Name = "Normal Item", SellIn = 5, Quality = 20 } };
+            var gildedRose = new GildedRose(items);
+
+            gildedRose.UpdateQuality();
+
+            Assert.AreEqual(19, items[0].Quality);
+        }
+    }
+}
